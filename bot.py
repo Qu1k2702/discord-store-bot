10b7ba7039b1f931faa4fdcdc0b0a7a00255b48b
Thales Henrique <thaleshenriquevieiragomes146@gmail.com>
2026-07-14 17:32:41 -0300

    Ephemerals se auto deletam

--- bot.py
+++ bot.py
@@ -12,6 +12,50 @@ intents = discord.Intents.default()
 intents.guilds = True
 intents.members = True
 
+_last_ephemeral = {}
+_ephemeral_tokens = {}
+_send_message = discord.InteractionResponse.send_message
+_followup_send = discord.Webhook.send
+
+
+async def _delete_last_ephemeral(user_id: int) -> None:
+    previous = _last_ephemeral.pop(user_id, None)
+    if previous is None:
+        return
+    try:
+        if isinstance(previous, discord.Interaction):
+            await previous.delete_original_response()
+        else:
+            await previous.delete()
+    except discord.HTTPException:
+        pass
+
+
+async def _tracked_send_message(self, *args, **kwargs):
+    interaction = self._parent
+    _ephemeral_tokens[interaction.token] = interaction.user.id
+    if kwargs.get("ephemeral", False):
+        kwargs.setdefault("delete_after", 5)
+    response = await _send_message(self, *args, **kwargs)
+    if kwargs.get("ephemeral", False):
+        await _delete_last_ephemeral(interaction.user.id)
+        _last_ephemeral[interaction.user.id] = interaction
+    return response
+
+
+async def _tracked_followup_send(self, *args, **kwargs):
+    user_id = _ephemeral_tokens.get(self.token)
+    message = await _followup_send(self, *args, **kwargs)
+    if kwargs.get("ephemeral", False) and user_id is not None:
+        await _delete_last_ephemeral(user_id)
+        _last_ephemeral[user_id] = message
+        await message.delete(delay=5)
+    return message
+
+
+discord.InteractionResponse.send_message = _tracked_send_message
+discord.Webhook.send = _tracked_followup_send
+
 
 class StoreBot(commands.Bot):
     def __init__(self) -> None:
